2215895941b20a47e6fbd43fdaf9598e89be4afc
ebitiem-oss <ebi.tiem@gmail.com>
2026-08-24 11:38:16 +0330

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function openGame(gameName) {
+    alert("در حال اجرای بازی: " + gameName);
+    console.log("User clicked on: " + gameName);
+}
+
+window.onload = function () {
+    console.log("Nivra Game Hub is ready!");
+};
+add app.js
